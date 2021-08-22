8bae8b79bc1c6dc7fed1acca28561ec0a6c6b3fd
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-08-22 19:09:20 +0200

    refactor dfr

--- dfr.py
+++ dfr.py
@@ -1,42 +1,67 @@
-from os.path import join
-from torrentool.api import Torrent
+from typing import Final
+import libtorrent as lt
 import os
 import shutil
 
-torrent_path = "/home/torrent/transmission/config/torrents"
-base_path = "/mnt/pool/downloads/completed/"
-paths = [base_path, os.path.join(base_path, "tv-sonarr"), os.path.join(base_path, "radarr")]
-ignore_folders = set(["tv-sonarr", "radarr", "moana"])
-found_files = set()
-completed_download_files = set()
+TORRENT_PATH: Final = "/var/lib/docker/volumes/transmission/_data/transmission-home/torrents"
+DOWNLOAD_PATH: Final = "/mnt/pool/downloads/completed"
+LABELS: Final = ["radarr", "tv-sonarr", ""]
+IGNORE_FOLDERS: Final = ["tv-sonarr", "radarr", "moana"]
 
-def remove_file(full_path):
-    if os.path.exists(full_path):
-        if os.path.isfile(full_path):
-            print(f"Lösche Datei: {full_path}")
-            #os.remove(full_path)
-        elif os.path.isdir(full_path):
-            print(f"Lösche Ordner: {full_path}")
-            #shutil.rmtree(full_path)
+def main():
+    dangling_files = find_dangling_files()
+    print_file_size(dangling_files)
+    # Set true to delete files
+    delete_files(dangling_files, True)
 
-for path in paths:
-    completed_download_files |= set(os.listdir(path))
+def find_dangling_files():
+    local_files = set([os.path.join(DOWNLOAD_PATH, file_name) for file_name in os.listdir(DOWNLOAD_PATH)])
+    local_files -= set([os.path.join(DOWNLOAD_PATH, file_name) for file_name in IGNORE_FOLDERS])
+    torrent_folders = get_all_torrent_folders()
+    return local_files.difference(torrent_folders)
 
-for torrent in os.listdir(torrent_path):
+def delete_files(files, delete=False):
+    for file in files:
+        if os.path.exists(file) and delete:
+            if os.path.isfile(file):
+                os.remove(file)
+            elif os.path.isdir(file):
+                shutil.rmtree(file)
+            print(f"Deleting file: {file} permanently")  
+        elif not delete:
+            print("Safe mode enabled please set delete to true")
+            return
+
+def print_file_size(paths):
+    size = 0
+    for path in paths:
+        for root, _, files in os.walk(path):
+            for file in files:
+                size += os.path.getsize(os.path.join(root, file))
+    
+    print(f"Total size: {size / 1_000_000_000} GB")
+
+def get_all_torrent_folders():
+    torrent_folders = set()
+    for torrent_file in os.listdir(TORRENT_PATH):
+        torrent_file = os.path.join(TORRENT_PATH, torrent_file)
+        if os.path.isfile(torrent_file) and torrent_file.endswith(".torrent"):
+            torrent_info = lt.torrent_info(torrent_file)
+            location = get_torrent_data_location(torrent_info.name())
+            if location != -1:
+                torrent_folders.add(location)
+    return torrent_folders
+
+def get_torrent_data_location(torrent_name):
+    index = 0
     try:
-        file = Torrent.from_file(os.path.join(torrent_path, torrent)).files[0].name.split("/")[0]
-        if file in completed_download_files: found_files.add(file)
+        while(not os.path.exists(location := os.path.join(DOWNLOAD_PATH, LABELS[index], torrent_name))):
+            index += 1
     except IndexError:
-        print(f"Torrent {torrent} konnte nicht verarbeitet werden!")
+        print(f"Unable to find location for torrent {torrent_name}")
+        location = -1
 
-files_to_delete = completed_download_files.difference(found_files) - ignore_folders
+    return location
 
-if len(files_to_delete) > 0:
-    print(f"Die folgenden Dateien können gelöscht werden:")
-    for file_to_delete in files_to_delete:
-        for path in paths:
-            if file_to_delete in os.listdir(path):
-                remove_file(os.path.join(path, file_to_delete))
-                break # Optimierung muss noch getestet werden
-else:
-    print(f"Es gibt keine Dateien, welche nicht einem Torrent zugeordnet werden können.")
+if __name__ == "__main__":
+    main()
