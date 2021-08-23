9d2cd1f1f527492549e8213de6aab381d8d7e3d0
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-08-23 14:52:34 +0200

    refactor unzipped file remover script

--- unzipped_file_remover.py
+++ unzipped_file_remover.py
@@ -1,4 +1,5 @@
 from typing import Final
+from os.path import join
 import libtorrent as lt
 import os
 import shutil
@@ -14,59 +15,39 @@ def main():
 
 def get_files_to_delete():
     files_to_delete = []
-    for torrent_file in os.listdir(TORRENT_PATH):
-        torrent_file = os.path.join(TORRENT_PATH, torrent_file)
+    for torrent_file in (join(TORRENT_PATH, tf) for tf in os.listdir(TORRENT_PATH)):
         if os.path.isfile(torrent_file) and torrent_file.endswith(".torrent"):
-            torrent_info = lt.torrent_info(torrent_file)
-            location = get_torrent_data_location(torrent_info.name())
-            if location != -1 and get_num_of_files(location) != torrent_info.num_files():
-                files_to_delete += [*find_files_not_in_torrent(location, torrent_info)]
+            try:
+                torrent_info = lt.torrent_info(torrent_file)
+                location = [loc for count, path in enumerate(LABELS) if (os.path.exists(loc := join(DOWNLOAD_PATH, LABELS[count], torrent_info.name())))]
+                if len(location) and get_num_of_files(location[0]) != torrent_info.num_files():
+                    files_to_delete += [*find_files_not_in_torrent(location[0], torrent_info)]
+            except RuntimeError:
+                print(f"An error occured while trying to read torrent {torrent_file}")
 
     return files_to_delete
 
-def delete_files(files):
-    for file in files:
-        if os.path.exists(file):
-            os.remove(file)
-            print(f"Deleting file: {file} permanently")  
+def delete_files(files, delete=False):
+    for file in (f for f in files if os.path.exists(f) and delete):
+        #os.remove(file)
+        print(f"Deleting file: {file} permanently")
+
+    if not delete: print("Files won't be deleted exiting")
 
 def get_file_size(torrents):
-    size = 0
-    for torrent in torrents:
-        size += os.path.getsize(torrent)
-    
+    size = sum([os.path.getsize(torrent) for torrent in torrents])
     print(f"Total size: {size / 1_000_000_000} GB")
 
 def find_files_not_in_torrent(location, torrent_info):
     torrent_files = set([torrent_info.files().file_path(index, location.replace(torrent_info.name(), "")) for index in range(torrent_info.num_files())])
-    files_on_disk = set()
+    files_on_disk = set([join(torrent_info.name(), root, file) for root, _, files in os.walk(location) for file in files])
 
-    for root, _, files in os.walk(location):
-        for file in files:
-            files_on_disk.add(os.path.join(torrent_info.name(), root, file))
-
-    return files_on_disk.difference(torrent_files)
-            
+    return files_on_disk.difference(torrent_files)        
 
 def get_num_of_files(location):
     total = 1
-    if os.path.isdir(location):
-        total = 0
-        for _, _, files in os.walk(location):
-            total += len(files)
-    
+    if os.path.isdir(location): total = sum([len(files) for _, _, files in os.walk(location)])
     return total
-
-def get_torrent_data_location(torrent_name):
-    index = 0
-    try:
-        while(not os.path.exists(location := os.path.join(DOWNLOAD_PATH, LABELS[index], torrent_name))):
-            index += 1
-    except IndexError:
-        print(f"Unable to find location for torrent {torrent_name}")
-        location = -1
-
-    return location
 
 if __name__ == "__main__":
     main()
